69146710cf7dbf8d29bfd372e0bde52657a6fedd
Bijanc9 <BijanEsnaashari@gmail.com>
2021-03-10 19:06:05 -0600

    Update script.js
    Edited to fix loop problem

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//Defines the character parameters and the resultant password as empty strings to start
 var characters='';
-var result='';
-function passwordlow() {var passwordlower=window.prompt("Include lowercase characters? (enter y or n)")
-    if (passwordlower !=='y'|| passwordlower!=='n'){window.alert("invalid entry, please try again");passwordlower='';passwordlow()}};;
+var result=''
+
+    
 // Write password to the #password input
 function writePassword() {
+  //Successive prompts to define the users chosen parameters
   var passwordlen=window.prompt("Please enter a length between 8 and 128 characters");
-  passwordlow();
-    
+  var passwordlower=window.prompt("Include lowercase characters? (enter y or n)");  
   var passwordupper=window.prompt("include upper case characters? (enter y or n)");
   var passwordnum=window.prompt("Include numeric characters? (enter y or n)");
   var passwordspecial=window.prompt("Include special characters? (enter y or n)");
+  //Variables defining the strings to be added to the character string
   var alpha='abcdefghijklmnopqrstuvwxyz';
   var alphaUpper='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  
+  var num = '1234567890';
+  var special='!@#$%^&*()?'
   var passwordText = document.querySelector("#password");
+  //checking user input and concantinating the character string based on that input
   if (passwordupper=='y'){characters+=alphaUpper};
-  if (passwordnum=='y'){characters+='1234567890'};
-  if (passwordspecial=='y'){characters+='!@#$%^&*()?'};
+  if (passwordnum=='y'){characters+=num};
+  if (passwordspecial=='y'){characters+=special};
   if (passwordlower=='y'){characters+=alpha};
+  //For loop to generate password as result variable
   for ( var i = 0; i < passwordlen; i++ ) {
   result += characters.charAt(Math.floor(Math.random() * characters.length))};
+  //displaying the password to the html page
   passwordText.value = result;
+  //resetting characters and result to empty strings so new parameters can be selected
   characters='';
   result='';
 }
